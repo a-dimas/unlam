b520fe9980d011a9b823f4712ec8ebce32ef312c
Dimas Aguirre <seymoure@gmail.com>
2017-05-19 19:27:38 -0300

    maximo y minimo

--- UNLaM/UNLaM/src/prolog/promocionaste.pl
+++ UNLaM/UNLaM/src/prolog/promocionaste.pl
@@ -9,3 +9,12 @@ parcial2(jose,3).
 promociona(ALUMNO):-parcial1(ALUMNO,NOTA1),parcial2(ALUMNO,NOTA2),NOTA1>=7,NOTA2>=7.
 cursada(ALUMNO):-parcial1(ALUMNO,NOTA1),parcial2(ALUMNO,NOTA2),NOTA1>=4,NOTA2>=4,(NOTA1+NOTA2)/2=<7.
 recursa(ALUMNO):-parcial1(ALUMNO,NOTA1),parcial2(ALUMNO,NOTA2),(NOTA1<4;NOTA2<4).
+prodCart(X,Y):-numero(X),numero(Y).
+numero(1).
+numero(2).
+numero(3).
+seleccion(X,Y):-prodCart(X,Y),X<Y.
+proyeccion(X):-seleccion(X,_).
+maximo(X):-numero(X),not(proyeccion(X)).
+
+minimo(X):-numero(X),not((prodCart(X,Y),X>Y)).
